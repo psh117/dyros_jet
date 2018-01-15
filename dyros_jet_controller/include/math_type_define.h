aaa943d2040831df69918893ed1a87d824e07fa8
Junhyung-Kim <john3.16@snu.ac.kr>
2018-01-15 17:20:59 +0900

    sudoinverse

--- dyros_jet_controller/include/math_type_define.h
+++ dyros_jet_controller/include/math_type_define.h
@@ -220,7 +220,7 @@ static Eigen::Vector3d rot2Euler(Eigen::Matrix3d Rot)
 template <typename _Matrix_Type_>
 _Matrix_Type_ pinv(const _Matrix_Type_ &a, double epsilon =std::numeric_limits<double>::epsilon())
 {
-    JacobiSVD< _Matrix_Type_ > svd(a ,ComputeThinU | ComputeThinV);
+    Eigen::JacobiSVD< _Matrix_Type_ > svd(a ,Eigen::ComputeThinU | Eigen::ComputeThinV);
     double tolerance = epsilon * max(a.cols(), a.rows()) *svd.singularValues().array().abs()(0);
 
     return svd.matrixV() *  (svd.singularValues().array().abs() > tolerance).select(svd.singularValues().array().inverse(), 0).matrix().asDiagonal() * svd.matrixU().adjoint();
